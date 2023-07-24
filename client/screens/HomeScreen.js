935585f1ea28c2ee9155163b4575e657e46fbc30
huybq_107 <64139999+huybq100701@users.noreply.github.com>
2023-07-24 13:43:40 +0700

    update

---
+++ client/screens/HomeScreen.js
@@ -0,0 +1,57 @@
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Lottie from 'lottie-react-native';
+import { useNavigation } from '@react-navigation/native';
+import { removeItem } from '../utils/asyncStorage';
+
+const {width, height} = Dimensions.get('window');
+
+export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  const handleReset = async ()=>{
+    await removeItem('onboarded');
+    navigation.push('Onboarding');
+  }
+  return (
+    <View  style={styles.container}>
+      <View style ={styles.heading}>
+      <TouchableOpacity onPress={handleReset} style={styles.resetButton}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Home Page</Text>
+      </View>
+
+      <View style={styles.lottie}>
+        <Lottie source={require('../assets/animations/food.gif')} autoPlay loop />
+      </View>
+      
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  heading:{
+    flexDirection: 'row',
+  },
+  lottie:{
+    width: width*0.9,
+    height: width
+  },
+  text: {
+    fontSize: width*0.06,
+    marginBottom: 20,
+    justifyContent: 'center',
+    marginLeft: width*0.2
+  },
+  resetButton: {
+    backgroundColor: '#34d399',
+    padding: 10,
+    borderRadius: 50
+  }
+})
