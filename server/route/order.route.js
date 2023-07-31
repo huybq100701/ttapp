0f1cbb7a2d0771ae8770bb6225bf7b2aebb1a43d
Tien Dung <tiendung.2001.dev@gmail.com>
2023-07-31 08:31:04 +0700

    order controller

---
+++ server/route/order.route.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const orderRouter = express.Router();
+
+const orderController = require('../controller/OrderController');
+
+orderRouter.post('/', orderController.createOrder);
+orderRouter.get('/:id', orderController.getOrdersById);
+
+module.exports = orderRouter;
