e125f58d259df283aea64e5cae18d30b57d39efb
huybq_107 <64139999+huybq100701@users.noreply.github.com>
2023-08-01 11:06:45 +0700

    fix ui

--- client/src/screens/UserScreen.js
+++ client/src/screens/UserScreen.js
@@ -1,60 +1,65 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, TextInput, StyleSheet, Dimensions } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { themeColors } from '../theme';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { Cog6ToothIcon } from 'react-native-heroicons/solid';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 export default function UserScreen() {
     const navigation = useNavigation();
     const insets = useSafeAreaInsets();
+
+
     const handleSetting = () => {
         navigation.navigate('Setting');
     }
-    
+
     return (
         <View style={[styles.container, { paddingTop: insets.top }]}>
             <StatusBar style={{ backgroundColor: themeColors.bg }} />
             <View style={styles.settings}>
                 <TouchableOpacity style={styles.settingButton} onPress={handleSetting}>
-                    <Cog6ToothIcon size={28} color="black" />
+                    <Cog6ToothIcon size={windowWidth * 0.07} color="black" />
                 </TouchableOpacity>
             </View>
-            <View style={styles.imageContainer}>
+            <View style={[styles.imageContainer, { height: windowHeight * 0.2 }]}>
                 <Image source={require('../../assets/images/bglogin.jpg')} style={styles.image} />
             </View>
             <View style={styles.usernameContainer}>
-                <Text style={styles.username}>Username</Text>
+                <Text style={[styles.username, { fontSize: windowWidth * 0.06 }]}>Username</Text>
             </View>
-            <View style={styles.formContainer}>
+            <View style={[styles.formContainer, { minHeight: windowHeight * 0.5 }]}>
                 <View style={styles.infoContainer}>
-                    <Text style={styles.label}>Infomation</Text>
+                    <Text style={[styles.label, { fontSize: windowWidth * 0.05 }]}>Information</Text>
                 </View>
                 <View style={styles.divider}></View>
                 <View style={styles.infoContainer}>
-                    <Text style={styles.label}>Email:</Text>
-                    <Text style={styles.content}>user@user.com</Text>
+                    <Text style={[styles.label, { fontSize: windowWidth * 0.04 }]}>Email:</Text>
+                    <Text style={[styles.content, { fontSize: windowWidth * 0.04 }]}>user@user.com</Text>
                 </View>
                 <View style={styles.divider}></View>
                 <View style={styles.infoContainer}>
-                    <Text style={styles.label}>Fullname:</Text>
-                    <Text style={styles.content}>Nguyễn Văn A</Text>
+                    <Text style={[styles.label, { fontSize: windowWidth * 0.04 }]}>Fullname:</Text>
+                    <Text style={[styles.content, { fontSize: windowWidth * 0.04 }]}>Nguyễn Văn A</Text>
                 </View>
                 <View style={styles.divider}></View>
                 <View style={styles.infoContainer}>
-                    <Text style={styles.label}>Birthday:</Text>
-                    <Text style={styles.content}>dd/mm/yyyy</Text>
+                    <Text style={[styles.label, { fontSize: windowWidth * 0.04 }]}>Birthday:</Text>
+                    <Text style={[styles.content, { fontSize: windowWidth * 0.04 }]}>dd/mm/yyyy</Text>
                 </View>
                 <View style={styles.divider}></View>
                 <View style={styles.infoContainer}>
-                    <Text style={styles.label}>Address:</Text>
-                    <Text style={styles.content}>Hai Phong, Vietnam</Text>
+                    <Text style={[styles.label, { fontSize: windowWidth * 0.04 }]}>Address:</Text>
+                    <Text style={[styles.content, { fontSize: windowWidth * 0.04 }]}>Hai Phong, Vietnam</Text>
                 </View>
                 <View style={styles.divider}></View>
                 <View style={styles.infoContainer}>
-                    <Text style={styles.label}>Phone:</Text>
-                    <Text style={styles.content}>0795256013</Text>
+                    <Text style={[styles.label, { fontSize: windowWidth * 0.04 }]}>Phone:</Text>
+                    <Text style={[styles.content, { fontSize: windowWidth * 0.04 }]}>0795256013</Text>
                 </View>
             </View>
         </View>
@@ -69,41 +74,36 @@ const styles = StyleSheet.create({
     settings: {
         justifyContent: 'flex-end',
         alignItems: 'flex-end',
-        paddingHorizontal: 16,
+        paddingHorizontal: windowWidth * 0.05,
         paddingTop: 16,
     },
     settingButton: {
         backgroundColor: '#F59E0B',
-        padding: 10,
+        padding: windowWidth * 0.03,
         borderRadius: 20,
     },
     imageContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        height: 200,
     },
     image: {
-        borderRadius: 100,
-        width: 200,
-        height: 200,
+        borderRadius: windowWidth * 0.2,
+        width: windowWidth * 0.4,
+        height: windowWidth * 0.4,
     },
     usernameContainer: {
         justifyContent: 'center',
         alignItems: 'center',
         padding: 10,
-        fontSize: 20,
-        color: '#4B5563',
-        fontWeight: 'bold',
     },
     username: {
-        fontSize: 28,
+        fontSize: windowWidth * 0.06,
         color: '#4B5563',
         fontWeight: 'bold',
     },
     formContainer: {
         backgroundColor: 'white',
         borderRadius: 32,
-        minHeight: 500,
         marginTop: 12,
         padding: 8,
         paddingTop: 20,
@@ -115,14 +115,10 @@ const styles = StyleSheet.create({
     },
     label: {
         flex: 2,
-        fontSize: 20,
-        color: '#4B5563',
         fontWeight: 'bold',
     },
     content: {
         flex: 5,
-        fontSize: 20,
-        color: '#4B5563',
     },
     divider: {
         backgroundColor: '#ccc',
